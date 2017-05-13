7005940294d4c49f193d431af2432f60229bb99c
Rik Nijessen <riknijessen@gmail.com>
2017-05-13 20:55:03 +0200

    doc2vec implementation

---
+++ hackdelft/doc2vec/train_doc2vec.py
@@ -0,0 +1,38 @@
+import logging
+from gensim.models.doc2vec import TaggedDocument, Doc2Vec
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+class Documents(object):
+    def __init__(self, documents):
+        self.documents = documents
+
+    def __iter__(self):
+        for i, doc in enumerate(self.documents):
+            yield TaggedDocument(words = doc, tags = [i])
+file = ""
+corpus = open(file, "r")
+lines = corpus.read().lower().split("\n")
+count = len(lines)
+preprocessed = []
+for t in lines:
+    fixed =''.join([x for x in t if x.isalnum() or x.isspace()]).split()
+    preprocessed.append(fixed)
+
+documents = Documents(preprocessed)
+
+
+
+
+#iter = 1, because we keep training ourselves :)
+model = Doc2Vec(size=200, dbow_words= 1, dm=0, iter=1,  window=5, seed=1337, min_count=5, workers=16,alpha=0.025, min_alpha=0.025)
+model.build_vocab(documents)
+for epoch in range(10):
+    print("epoch "+str(epoch))
+    model.train(documents, total_examples=count, epochs=1)
+    model.save(config._current_path+'/doc2vec_models/'+file+str(epoch)+'.model')
+    model.alpha -= 0.002  # decrease the learning rate
+    model.min_alpha = model.alpha  # fix the learning rate, no decay
+
+
+
